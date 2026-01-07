880ac8163a07cada5d688ba0e6a384f39cbb54b4
bruecknerlina <brueckner.lina@gmail.com>
2026-01-07 11:47:41 +0100

    Update __init__.py

--- era5vis-main/era5vis/__init__.py
+++ era5vis-main/era5vis/__init__.py
@@ -1,6 +1,10 @@
+""" Updated by Lina Brückner, January 2026:
+    - import write_scalar_with_wind_html and write_skewT_html instead of write_html
+"""
+
 # This is a hard coded version string.
 # Real packages use more sophisticated methods to make sure that this
 # string is synchronised with `setup.py`, but for our purposes this is OK
 __version__ = '0.0.1'
 
-from era5vis.core import write_html
+from era5vis.core import write_scalar_with_wind_html, write_skewT_html
