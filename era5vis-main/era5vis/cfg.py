eec48fde1f06f4456dd56e4c65cff28f2b9cb0d9
bruecknerlina <brueckner.lina@gmail.com>
2026-01-07 12:42:31 +0100

    Update cfg.py

--- era5vis-main/era5vis/cfg.py
+++ era5vis-main/era5vis/cfg.py
@@ -1,11 +1,15 @@
-""" Configuration module containing settings and constants. """
+"""
+    Configuration module containing settings and constants.
+    Updated by Lina Brückner, January 2026:
+        - seperate into two possible datasets
+"""
 
 from pathlib import Path
-
 
 
 # location of data directory containing html template
 pkgdir = Path(__file__).parents[0]
 html_template = Path(pkgdir) / 'data' / 'template.html'
 
-datafile = Path(pkgdir) / 'data' / 'era5_example_dataset.nc'
+scalar_wind_datafile = Path(pkgdir) / 'data' / 'era5_example_dataset.nc'
+skewT_datafile      = Path(pkgdir) / 'data' / 'era5_example_1.nc'
