22f071eb4075ae9905a4603e2cea7452d2827130
bruecknerlina <brueckner.lina@gmail.com>
2026-01-09 16:44:42 +0100

    Update cli.py

--- era5vis-main/era5vis/cli.py
+++ era5vis-main/era5vis/cli.py
@@ -101,7 +101,7 @@ def _parse_args(args):
         )
     )
     parser.add_argument(
-        "pl", "--plot_type",
+        "-pl", "--plot_type",
         choices=["scalar_wind","skewT"],
         type=str,
         default="scalar_wind",
