177b6219b581935c9cb7e1a76276157ff3ff8493
bruecknerlina <brueckner.lina@gmail.com>
2026-01-12 10:19:35 +0100

    Update test_era5.py

--- era5vis-main/era5vis/tests/test_era5.py
+++ era5vis-main/era5vis/tests/test_era5.py
@@ -22,8 +22,8 @@ def test_horiz_cross_section(retrieve_param_level_time_from_ds, datafile):
     assert np.datetime64(da.valid_time.item(), 'ns') == time
 
     # test using valid_time
-    # convert np.datetime64 to string for horiz_cross_section (it expects str)
-    time_str = np.datetime_as_string(time, unit='m')  # e.g., '2025-10-01T00:00'
+    # convert np.datetime64 to string for horiz_cross_section
+    time_str = np.datetime_as_string(time, unit='m')
     da2 = era5.horiz_cross_section(param=param, lvl=level, time=time_str, datafile=datafile)
     assert isinstance(da2, xr.DataArray)
     assert set(da2.dims) == {'latitude', 'longitude'}
